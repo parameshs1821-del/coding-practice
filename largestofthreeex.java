d8ee51110c944c8a841df9dec5216c7e1de1dfc6
parameshs1821-del <parameshs1821@gmail.com>
2026-03-18 22:37:32 +0530

    Remove unnecessary files

---
+++ largestofthreeex.java
@@ -0,0 +1,17 @@
+public class largestofthreeex {
+    public static void main(String[] args){
+        int a =10;
+        int b = 20;
+        int c = 35;
+        if(a > b && a >c ){
+            System.out.println("A is largest value");
+        }else if(b > a && b > c) {
+            System.out.println("B is the largest value");
+        }else{
+            System.out.println("C is the largest value");
+        }
+
+        }
+    }
+    
+
---
+++ largestoftwoex.java
@@ -0,0 +1,12 @@
+public class largestoftwoex {
+    public static void main(String [] args){
+        int a =10;
+        int b= 5;
+        if(a > b){
+            System.out.println("A is largest value");
+        }else{
+            System.out.println("B is largest value");
+        }
+    }
+    
+}
