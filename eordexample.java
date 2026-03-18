f7349d6588216cb22f244be795fc18d18df9efa2
parameshs1821-del <parameshs1821@gmail.com>
2026-03-18 18:24:10 +0530

    Initial commit

---
+++ eordexample.java
@@ -0,0 +1,11 @@
+public class eordexample{
+    public static void main(String[] args) {
+        int b = 10;
+        if (b % 5 == 0){
+            System.out.println("The number is Even");
+        }else{
+            System.out.println("The number is odd");
+        }
+        }
+        
+    }
