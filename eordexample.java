c5efeb8012c9fd284088aad89b97918818d57e58
parameshs1821-del <parameshs1821@gmail.com>
2026-03-18 19:47:38 +0530

    Updated Java practice files

--- eordexample.java
+++ eordexample.java
@@ -1,7 +1,7 @@
 public class eordexample{
     public static void main(String[] args) {
-        int b = 10;
-        if (b % 5 == 0){
+        int b = 11;
+        if (b % 2 == 0){
             System.out.println("The number is Even");
         }else{
             System.out.println("The number is odd");
---
+++ smallorlargeex.java
@@ -0,0 +1,13 @@
+public class smallorlargeex {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 20;
+        if (a < b){
+            System.out.println( "b is biggest value");
+        }else{
+            System.out.println("a is smallest value ");
+        }
+
+    }
+    
+}
