ad9a6498bb6a9e548fd93f2ef572f51555266367
parameshs1821-del <parameshs1821@gmail.com>
2026-03-18 20:08:49 +0530

    Updated Java practice files

--- smallorlargeex.java
+++ smallorlargeex.java
@@ -2,10 +2,10 @@ public class smallorlargeex {
     public static void main(String[]args){
         int a = 10;
         int b = 20;
-        if (a < b){
-            System.out.println( "b is biggest value");
+        if (a > b){
+            System.out.println( "a is largest value");
         }else{
-            System.out.println("a is smallest value ");
+            System.out.println("b is largest value ");
         }
 
     }
