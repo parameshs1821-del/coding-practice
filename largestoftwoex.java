8bfa4385d3ac92faa0fd62282d24e3899ddba5be
parameshs1821-del <parameshs1821@gmail.com>
2026-03-20 21:33:51 +0530

    Updated Java practice files

--- largestoftwoex.java
+++ largestoftwoex.java
@@ -1,11 +1,12 @@
 public class largestoftwoex {
     public static void main(String [] args){
-        int a =10;
-        int b= 5;
-        if(a > b){
-            System.out.println("A is largest value");
-        }else{
-            System.out.println("B is largest value");
+int a = 20;
+int b = 30;
+if (a > b ){
+    System.out.println("A is largest");
+}
+        else{
+            System.out.println("B is largest");
         }
     }
     
