3a5178d3c26be00450ab9ba96537e99f9d39b793
parameshs1821-del <parameshs1821@gmail.com>
2026-03-20 21:52:37 +0530

    updated Java practice files

--- positivenegativezero.java
+++ positivenegativezero.java
@@ -1,13 +1,15 @@
 public class positivenegativezero {
     public static void main(String []args){
-        int num = 1;
-        if (num > 0){
-            System.out.println("the number is positive");
-        }else if(num < 0){
-            System.out.println("the number is negative");
+        int a = 10 ;
+        if(a > 0){ 
+            System.out.println("value is positive");
+
+        }else if(a < 0){
+            System.out.println("value is negative");
         }else{
-            System.out.println("the number is Zero");
+            System.out.println("value is zero");
+        }
         }
     }
     
-}
+
