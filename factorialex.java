e37f24256705bcc7ed480354bbd1df27854d8b87
parameshs1821-del <parameshs1821@gmail.com>
2026-03-21 23:47:09 +0530

    updated Java practice files

---
+++ factorialex.java
@@ -0,0 +1,11 @@
+public class factorialex {
+    public static void main(String[] args){
+     int n = 5;
+      int fact =  2 ;
+      for (int i = 1; i <=n ; i++){
+        fact = fact * i ;
+      }
+      System.out.println("factorial is: " + fact );
+    }
+    
+}
