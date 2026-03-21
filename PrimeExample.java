7c3e8cbd4a64457416f906b12703d21551548316
parameshs1821-del <parameshs1821@gmail.com>
2026-03-21 23:55:53 +0530

    updated Java practice files

---
+++ PrimeExample.java
@@ -0,0 +1,24 @@
+public class PrimeExample {
+    public static void main(String[] args) {
+
+        int n = 7;
+        boolean isPrime = true;
+
+        if(n <= 1){
+            isPrime = false;
+        } else {
+            for(int i = 2; i < n; i++){
+                if(n % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if(isPrime){
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Number");
+        }
+    }
+}
